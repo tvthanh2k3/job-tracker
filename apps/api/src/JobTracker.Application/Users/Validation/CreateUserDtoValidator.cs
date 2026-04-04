c3a7848061b7ef249d3489cf4592963a570c398c
tvthanh2k3 <tvthanh1201@gmail.com>
2026-04-04 23:31:24 +0700

    feat: add user validators

---
+++ apps/api/src/JobTracker.Application/Users/Validation/CreateUserDtoValidator.cs
@@ -0,0 +1,22 @@
+using FluentValidation;
+using JobTracker.Application.Users.Dto;
+
+namespace JobTracker.Application.Users.Validation;
+
+public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+{
+    public CreateUserDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
+
+        RuleFor(x => x.FullName)
+            .NotEmpty().WithMessage("Full Name is required.")
+            .MaximumLength(100).WithMessage("Full Name must not exceed 100 characters.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+    }
+}

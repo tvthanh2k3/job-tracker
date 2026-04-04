4df6609d443d7131b1692b9fb1c2efe023f5384b
tvthanh2k3 <tvthanh1201@gmail.com>
2026-04-05 00:03:39 +0700

    feat: add interview dtos

---
+++ apps/api/src/JobTracker.Application/Interviews/Dto/UpdateInterviewDto.cs
@@ -0,0 +1,9 @@
+namespace JobTracker.Application.Interviews.Dto
+{
+    public class UpdateInterviewDto
+    {
+        public DateTime ScheduledAt { get; set; }
+        public string Round { get; set; } = string.Empty;
+        public string? Notes { get; set; }
+    }
+}

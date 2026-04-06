2ef4aad594080a1f0ede1e86987dc2a1408e9e78
tvthanh2k3 <tvthanh1201@gmail.com>
2026-04-06 18:39:29 +0700

    feat: add auth controller and middleware

--- apps/api/src/JobTracker.Api/Program.cs
+++ apps/api/src/JobTracker.Api/Program.cs
@@ -1,29 +1,25 @@
-using JobTracker.Infrastructure;
-using JobTracker.Application;
+using JobTracker.Api;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddInfrastructureServices(builder.Configuration);
-builder.Services.AddApplicationServices();
+// Add services to the container by calling the Installer
+builder.Services.InstallServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.MapOpenApi();
-	app.MapScalarApiReference();
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowFrontend");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

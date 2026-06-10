bee459a097a8af8be369bf2772fdbaa4d096c46b
tvthanh2k3 <tvthanh1201@gmail.com>
2026-06-10 14:56:07 +0700

    feat(interviews): add interviews migration

---
+++ apps/api/src/JobTracker.Infrastructure/Migrations/20260610075226_Update_Interviews_Table.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Interviews_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Interviews",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Interviews");
+        }
+    }
+}
